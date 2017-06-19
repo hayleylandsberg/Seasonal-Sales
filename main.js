7b8af90f39c5bfbc0be1890e7c69e4c90eabd3e9
Hayley Landsberg <hayleyheath@Hayleys-MacBook-Air.local>
2017-06-19 14:38:19 -0500

    exercise  completed in class

---
+++ main.js
@@ -0,0 +1,30 @@
+console.log("Yup, it loaded");
+
+//get products
+function displayProducts() {
+	let prodArr = JSON.parse(event.target.responseText).products;
+	console.log("products array", prodArr);
+}
+
+function displayCategories() {
+	let catArr = JSON.parse(event.target.responseText).categories;
+	console.log("category array", catArr);
+}
+
+function getCategories() {
+	//displayProducts(event.target.responseText);
+	let reqCategories = new XMLHttpRequest();
+	reqCategories.addEventListener("load", displayCategories)
+	reqCategories.open("GET", "data/categories.json");
+	reqCategories.send();
+}
+
+function getProducts() {
+	let reqProducts = new XMLHttpRequest();
+	reqProducts.addEventListener("load", displayProducts)
+	reqProducts.open("GET", "data/products.json");
+	reqProducts.send();
+}
+
+getProducts();
+getCategories();
